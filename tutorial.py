fafc1d081edbc83d99d86a5061194e00e895f33e
rizzabh <pandeyrishabh966@gmail.com>
2024-01-07 22:16:17 +0530

    imports

--- tutorial.py
+++ tutorial.py
@@ -1,1 +1,23 @@
-#pygame
+import os
+import random
+import math
+import sys
+import pygame
+from os import listdir
+from os.path import isfile, join
+pygame.init()
+
+pygame.display.set_caption("Platformer")
+
+WIDTH, HEIGHT = 1000, 800
+FPS = 60
+PLAYER_VEL = 5
+
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+
+if __name__ == "__main__":
+    main(window)
+
+
+#Ansh&Rishabh           
